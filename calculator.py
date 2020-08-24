78fbbe68447457e4ab369f2bb5376122c7b2c61b
Yi Wang <yw374@duke.edu>
2020-08-24 12:55:15 -0400

    change messege of the interface

--- calculator.py
+++ calculator.py
@@ -3,7 +3,7 @@ def interface():
     while True:
         print("Options:")
         print("9 - Quit")
-        choice = input("Enter your choice: ")
+        choice = input("Enter my choice: ")
         if choice=='9':
             return
 
